68dd469c18902ed0c10913d110ea7feddb760328
Anne Hawley Brett <annehawleybrett@gmail.com>
2016-08-23 21:45:19 -0400

    add app/index.js make a request/response; add env vars for deploy to heroku

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-require('express');
+var express = require('express');
+var app = express();
+
+var rules = require('./app');
+app.use(rules);
+
+var port = process.env.PORT || "8080";
+var host = process.env.HOST || "0.0.0.0";
+
+app.listen(port, host);
+console.log("listening on 0.0.0.0.8080...")
